c96040e48b7c08c7abdfc5918eb4243ec3a3ab28
datphamm <116443202+datphamm@users.noreply.github.com>
2024-05-25 11:02:51 +0700

    Add files via upload

---
+++ Student.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai2_Lab2
+{
+    internal class Student
+    {
+        private int ID;
+        private string Khoa;
+        private string TenSV;
+        private float DiemTB;
+
+        public Student() //Default constructor
+        {
+            ID = 1;
+            TenSV = "Pham Xuan Dat";
+            Khoa = "CNTT";
+            DiemTB = 7;
+        }
+
+        public Student(Student stu) //Copy constructor
+        {
+            ID = stu.ID;
+            TenSV = stu.TenSV;
+            Khoa = stu.Khoa;
+            DiemTB = stu.DiemTB;
+        }
+
+        //Constructor with parameters
+        public Student(int id, string ten, string kh, float dtb)
+        {
+            ID = id;
+            TenSV = ten;
+            Khoa = kh;
+            DiemTB = dtb;
+        }
+
+        public int GetStudentId() => ID;
+        public void SetStudentId(int value) => ID = value;
+
+        public string getName() => TenSV;
+        public void setName(string value) => TenSV = value;
+
+        public string GetFaculty() => Khoa;
+        public void SetFaculty(string value) => Khoa = value;
+
+        public float GetMark() => DiemTB;
+        public void SetMark(float value) => DiemTB = value;
+
+        public void Show()
+        {
+            Console.WriteLine("ID: " + GetStudentId());
+            Console.WriteLine("Họ và tên: " + getName());
+            Console.WriteLine("Khoa: " + GetFaculty());
+            Console.WriteLine("Điểm trung bình: " + GetMark());
+        }
+
+    }
+
+}
+   
+
---
+++ Student3.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Bai3_Lab1
+{
+    internal class Student3
+    {
+        private int SID;
+        private string Khoa;
+        private float DiemTB;
+
+        public Student() //Default constructor
+        {
+            SID = 1;
+            Name = "Pham Xuan Dat";
+            Khoa = "CNTT";
+            DiemTB = 7;
+        }
+
+        public Student(Student stu) //Copy constructor
+        {
+            SID = stu.SID;
+            Name = stu.Name;
+            Khoa = stu.Khoa;
+            DiemTB = stu.DiemTB;
+        }
+
+        //Constructor with parameters
+        public Student(int id, string ten, string kh, float dtb)
+        {
+            SID = id;
+            Name = ten;
+            Khoa = kh;
+            DiemTB = dtb;
+        }
+
+        public int GetStudentId() => SID;
+        public void SetStudentId(int value) => SID = value;
+
+        public string GetFaculty() => Khoa;
+        public void SetFaculty(string value) => Khoa = value;
+
+        public float GetMark() => DiemTB;
+        public void SetMark(float value) => DiemTB = value;
+
+        public void Show()
+        {
+            Console.WriteLine("SID: " + GetStudentId());
+            Console.WriteLine("Họ và tên: " + Name);
+            Console.WriteLine("Khoa: " + GetFaculty());
+            Console.WriteLine("Điểm trung bình: " + GetMark());
+        }
+
+    }
+
+}
+
